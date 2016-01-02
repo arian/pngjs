a9dd543474f8d6703d56699fa6d6ee4e73d4b66d
sunlianghua <me@ioslh.com>
2016-01-02 16:00:07 +0800

    function error is not defined

--- stream.js
+++ stream.js
@@ -207,6 +207,12 @@ var DecodeStream = (function DecodeStreamClosure() {
 })();
 
 var FlateStream = (function FlateStreamClosure() {
+
+  function error(msg){
+    throw new Error(msg);
+  }
+
+
   var codeLenCodeMap = new Uint32Array([
     16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15
   ]);
